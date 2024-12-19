1f5c55f981dc2e718f82c404b375d3be90aeead5
amryyahya <amryyahya07@gmail.com>
2024-12-19 08:59:34 +0000

    add timer

--- server.js
+++ server.js
@@ -15,12 +15,41 @@ const server = app.listen(1337, () => {
 });
 
 const io = socketio(server)
+const rooms = {};
 
 io.on('connection', (socket) => {
   socket.on('JoinRoom', (room) => {
     socket.join(room);
     console.log(`${socket.displayName} joined room: ${room}`);
     socket.to(room).emit('message', `User ${socket.displayName} has joined the room`);
+    if (!rooms[room]) {
+      rooms[room] = {
+        remainingTime: 1000,  // Initial timer value in seconds
+        isRunning: false,
+        lastUpdated: null,
+      };
+    }
+    socket.emit("updateTimer", rooms[room]);
+  });
+
+  socket.on("startTimer", (room) => {
+    if (rooms[room] && !rooms[room].isRunning) {
+      rooms[room].isRunning = true;
+      rooms[room].lastUpdated = Date.now();
+
+      io.to(room).emit("updateTimer", rooms[room]); // Notify all in the room
+    }
+  });
+
+  socket.on("pauseTimer", (room) => {
+    if (rooms[room] && rooms[room].isRunning) {
+      const elapsedTime = Math.floor((Date.now() - rooms[room].lastUpdated) / 1000);
+      rooms[room].remainingTime -= elapsedTime;
+      rooms[room].isRunning = false;
+      rooms[room].lastUpdated = null;
+
+      io.to(room).emit("updateTimer", rooms[room]); // Notify all in the room
+    }
   });
 
   socket.on('SetDisplayName', (displayName) => {
